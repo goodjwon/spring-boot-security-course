22a7ec8c4d9962ea01e794e996b4965b434b5f44
JungWon Park <firstman9@protonmail.com>
2021-02-28 12:03:06 +0900

    Spring 보안수업

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,27 @@
+package com.example.demo.student;
+
+
+public class Student {
+
+    private final int studentId;
+    private final String name;
+    private final int age;
+
+    public Student(int studentId, String name, int age) {
+        this.studentId = studentId;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
