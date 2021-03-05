aac1e9f53c0bce5d74460829a3bea7520b06f913
JungWon Park <firstman9@protonmail.com>
2021-03-05 11:30:27 +0900

    config 추가

--- src/main/java/com/example/demo/sercurity/ApplicationSercurityConfig.java
+++ src/main/java/com/example/demo/sercurity/ApplicationSercurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.sercurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.demo.sercurity.ApplicationUserPermission.*;
 import static com.example.demo.sercurity.ApplicationUserRole.*;
 
 @Configuration
@@ -33,6 +35,10 @@ public class ApplicationSercurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAnyAuthority((COURSE_WRITE.name()))
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAnyAuthority((COURSE_WRITE.name()))
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAnyAuthority((COURSE_WRITE.name()))
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -45,19 +51,22 @@ public class ApplicationSercurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmitUser = User.builder()
                 .username("annna")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+//                .roles(STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())
+//                .roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN_TRAINEE.name())
+//                .roles(ADMIN_TRAINEE.name())
+                .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
                 .build();
 
         return new InMemoryUserDetailsManager(
