1c4000c7afa772bd7f6c944245b36530a9c20cc1
JungWon Park <firstman9@protonmail.com>
2021-03-07 11:00:58 +0900

    csrf token

--- src/main/java/com/example/demo/sercurity/ApplicationSercurityConfig.java
+++ src/main/java/com/example/demo/sercurity/ApplicationSercurityConfig.java
@@ -34,9 +34,9 @@ public class ApplicationSercurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
-//                .csrf().disable()
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
